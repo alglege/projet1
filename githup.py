ce1959184d251fe3e2685416a35d05edeee66fae
alglege <algleges@gmail.com>
2023-09-11 17:00:14 +0100

    terminer

---
+++ githup.py
@@ -0,0 +1,12 @@
+#projet=[1,2,34,69,87]
+nombre= int(input("veuillez saisir  des nombres separes par des virgules svp"))
+
+add=True
+reponse=0
+#for l in nombre :
+while add :
+    nombresep =nombre.split("\n")
+    reponse=nombresep.append 
+    reponse=reponse+1
+print(reponse)
+   
